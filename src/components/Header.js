6ff8397b03526c86888e810cac0fb6d37f6abd51
John <jyk595@gmail.com>
2021-08-02 12:42:49 -0700

    Added navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 function Header () {
-    return (
-        <>
-        THIS SI THE HEADER
-        </>
-    )
+  return (
+    <nav>
+        <NavLink exact to="/"><h1>Restaurant Pal</h1></NavLink>
+        <div className="navBar">
+          <NavLink exact to="/login">Login</NavLink>
+          <NavLink exact to="/create-page">Get Started</NavLink>
+        </div>
+    </nav>
+  )
 }
 
 export default Header

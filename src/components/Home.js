84f6d7f3a8b9c77f94fb80fd803a99c459253a24
John <jyk595@gmail.com>
2021-08-04 14:00:25 -0700

    new photos to hp

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,22 +8,22 @@ function Home () {
             <div className="hp-banner"></div>
             <div className="hp-banner-text">
                 <h2 className="hp-banner-intro">Create a simple restaurant site in minutes.</h2>
-                <Link className="hp-banner-cta" exact to="/create-page">Get Started!</Link>
+                <Link className="hp-banner-cta" exact to="/create-account">Create an Account</Link>
             </div>
             <div className="mission-banner">
                 <h4 className="small-header">Our Mission</h4>
                 <p className="hero-text">We’re here to help you create a quick, simple & effective website for your restaurant. We know restauranteurs aren’t really techies, so we wanted to create a simple landing page for you to promote your business.</p>
-                <Link className="styled-button" exact to="/create-page">Get Started</Link>
+                <Link className="styled-button" exact to="/create-account">Create an Account</Link>
             </div>
             <div className="sample-banner">
                 <h4 className="small-header">View Some Samples</h4>
                 {/* Might want to do a map here on the sample websites */}
                 <div className="sample-container">
                     <div className="sample-container">
-                        <Link to="/restaurant/:id">
-                            <img src="https://i.ibb.co/cXbtQgL/Image-2021-08-02-at-2-26-29-PM.jpg" alt="Sliding"/>
-                            <p className="hero-text">Momofuku</p>
-                            <p className="location-text">New York, NY</p>
+                        <Link to="/restaurant/3">
+                            <img src="https://p69.f3.n0.cdn.getcloudapp.com/items/yAuyPl7v/77673293-c38d-4b03-8613-7be6429cd60d.jpg?source=viewer&v=23a5767e6ecbd4311cb6d93f7ef560a1" alt="Sliding"/>
+                            <p className="hero-text">Rakkan</p>
+                            <p className="location-text">Los Angeles, CA</p>
                         </Link>
                     </div>
                 </div>

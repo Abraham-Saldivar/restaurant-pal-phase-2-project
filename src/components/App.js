112452607311c0ad26e55fcb5cf9ac675ed8274b
John <jyk595@gmail.com>
2021-08-03 20:21:18 -0700

    add back useeffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
